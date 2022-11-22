84d854f46543d9b776727b6eb7812be8cd34d90a
Jeongwon-Ryu <113072586+Jeongwon-Ryu@users.noreply.github.com>
2022-11-23 00:13:15 +0900

    bingo

--- bingoBoard.h
+++ bingoBoard.h
@@ -1,9 +1,14 @@
+#define N_LINE 2
 #define N_SIZE 5//main.c와 bingoBoard.c에서 수행할 거니까 헤더에다가 써놓는 것이 좋음. 
+#define BINGO_NUMSTATUS_ABSENT -1
+#define BINGO_NUMSTATUS_PRESENT 0
+
 
 void bingo_init(void);
-void bingo_print(void);
+void bingo_printBoard(void);
 void bingo_inputNum(int sel);
-int bingo_coundCompletedLine(void); //naming convention 규칙 잡기 ex. 언더바로 시작.
+int bingo_countCompletedLine(void); //naming convention 규칙 잡기 ex. 언더바로 시작.
+int bingo_checkNum(int selNum);
 
 
  
--- main.c
+++ main.c
@@ -3,10 +3,44 @@
 #include <time.h>
 #include "bingoBoard.h" 
 
+#define BINGO_RES_UNFINISHED	-1
+#define BINGO_RES_FINISHED		0
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int check_gameEnd(void)
+{
+	int res = BINGO_RES_UNFINISHED;
+	if (bingo_countCompletedLine() >= N_LINE )
+		res = BINGO_RES_FINISHED;
+		
+	return res;
+}
+
+int get_number(void)
+{
+	
+	int selNum=0;
+
+	do {
+		printf("select a number :");
+		scanf("%d", &selNum);
+		fflush (stdin); //프로그래밍 입출력 과정에서 버퍼(입력의 찌끄래기)를 비우는 역할 
+		
+		if (selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT)
+		{
+			printf("%i is not on the board! select other one.\n", selNum);
+		}
+	} while (selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT);
+	
+	return selNum;	
+		
+}
+
 
 int main(int argc, char *argv[]) {
+	
+	int selNum;
+	int cnt;
 	
 	srand((unsigned)(time(NULL)));
 	
@@ -20,26 +54,27 @@ int main(int argc, char *argv[]) {
 	//game
 	
 	bingo_init();
-	bingo_printBoard();
-	bingo_inputNum(21);
-	bingo_printBoard();
 	
 	
 	//initialize bingo board
-	/*
-	while (game is not end) //줄수가 N_BINGO보다 작음 //while문에서 빠져나가는 조건: 게임이 끝나는 조건 
-	(
+	
+	while (check_gameEnd() == BINGO_RES_UNFINISHED) /*while (game is not end) //줄수가 N_BINGO보다 작음 //while문에서 빠져나가는 조건: 게임이 끝나는 조건*/ 
+	{
 		//bingo board 출력 : 숫자를 입력할 때마다 나의 bingo board가 달라짐.
-		
-		
+		bingo_printBoard();
+	
 		//print no. of completed lines
+		printf("No. of completed line : %i\n", bingo_countCompletedLine());
 		
 		//select a proper number //main 함수에서 직접 구현 
+		selNum = get_number();
 		
 		//update the board status: X를 넣고 나의 bingo board가 달라짐. 
-	)
-	*/	
+		bingo_inputNum(selNum);
+	}
+		
 	
+
 
 
 	//ending
@@ -50,6 +85,8 @@ int main(int argc, char *argv[]) {
 	printf("  Y O U W I N~  Y O U W I N~  Y O U W I N~  \n");
 	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
 	printf("############################################\n\n");
+	
+	bingo_printBoard();
 
 	return 0;
 }
